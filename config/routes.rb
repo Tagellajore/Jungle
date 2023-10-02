828a5129311688d0a5de1d24ca717f6cae420216
Tagellajore <tagelsft9@gmail.com>
2023-10-02 01:21:36 -0400

    admin-categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  
+
   root to: 'products#index'
   get 'about/' , to: 'about#index'
 
@@ -16,6 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
